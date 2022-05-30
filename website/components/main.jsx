d18b688598e35a666124ca06bef4a2c5a0bac6b5
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2022-05-31 03:08:07 +0530

    Added Ft Support

--- website/components/main.jsx
+++ website/components/main.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import MainStyle from "./main.style";
+import axios from "axios";
 
 function Main() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    axios.get("https://cf-challenge.dftechs.workers.dev/ipinfo").then(res => {
+      setData(res.data);
+      setLoading(false);
+    });
+  }, [])
+
+  if(!loading) {
   return (
     <MainStyle>
       <div className="content">
@@ -12,18 +24,48 @@ function Main() {
             className="profile-img"
           />
         </div>
-        <div className="name">Alliance Broadband Services Pvt.</div>
-        <div className="description">
-          WB : IN : AS
+        <div className="name">
+          {data?.asOrganization} ,  {data?.city}
         </div>
         <div className="description">
-          88.38320 : 22.51800
+        {data?.region} / {data?.country} / {data?.continent}
         </div>
         <div className="description">
-          Asia/Kolkata
+         Lang :- {data?.longitude} | Lat :- {data?.latitude}
+        </div>
+        <div className="description">
+          Timezone :- {data?.timezone}
         </div>
       </div>
     </MainStyle>
   );
+  }
+  else {
+    return (
+      <MainStyle>
+        <div className="content">
+          <div className="profile">
+            <img
+              src="/images/hero.jpg"
+              alt="Hero Banner Image"
+              className="profile-img"
+            />
+          </div>
+          <div className="name">
+            Data
+          </div>
+          <div className="description">
+            State : Country : Continent
+          </div>
+          <div className="description">
+            Longitude : Latitude
+          </div>
+          <div className="description">
+            Timezone
+          </div>
+        </div>
+      </MainStyle>
+    );
+  }
 }
 export default Main;

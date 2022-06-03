0b8f598c3170412f46492d8c21b2d87d6606bf5a
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2022-06-03 23:24:23 +0530

    ......

--- worker/src/index.js
+++ worker/src/index.js
@@ -5,12 +5,9 @@ import {
   customAlphabet
 } from 'nanoid';
 
-const nanoid = customAlphabet(
-  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-  6,
-);
+import CryptoJS from 'crypto-js';
 
-const headers = {
+const reqHeaders = {
   "Content-Type": "application/json; charset=utf-8",
   'Access-Control-Allow-Credentials': 'true',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -18,12 +15,17 @@ const headers = {
   'Access-Control-Allow-Headers': 'Content-Type'
 }
 
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  7,
+);
+
 const router = Router()
 
 router.get("/ipinfo", (request) => {
   const returnData = JSON.stringify(request.cf, null, 2);
   return new Response(returnData, {
-    headers: headers
+    headers: reqHeaders
   })
 })
 
@@ -34,14 +36,67 @@ router.post('/add', async (request) => {
     data = await request.json()
     toStore = {
       key: nanoid(),
-      value: data.data
+      value: data.data,
+      password: data.password
     }
   }
-  // await BRAVO68WEB.put(toStore.key, base64encode(toStore.value))
+  var encrypted = ""
+
+  if(toStore.password){
+    encrypted = CryptoJS.AES.encrypt(toStore.value, toStore.password);
+    // var decrypted = CryptoJS.AES.decrypt(encrypted, toStore.password);
+  }
+  else{
+    encrypted = CryptoJS.AES.encrypt(toStore.value, "SecretPassphrase");
+    // var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+  }
+
+  // console.log(decrypted.toString(CryptoJS.enc.Utf8));
+
+  // console.log(atob(t))
+  // data.data = base64encode(data.data)
+
+  await BRAVO68WEB.put(toStore.key,encrypted.toString())
 
   const returnData = JSON.stringify(data, null, 2);
   return new Response(returnData, {
-    headers: headers
+    headers: reqHeaders
+  })
+})
+
+router.post('/fetch', async (request) => {
+  let data = {}
+  let toFetch = {}
+  if (request.headers.get("Content-Type") === "application/json") {
+    data = await request.json()
+    toFetch = {
+      key: data.key,
+      password: data.ppassword
+    }
+  }
+  var decrypted = ""
+
+  await BRAVO68WEB.get(toFetch.key)
+
+  if(toFetch.password){
+    // encrypted = CryptoJS.AES.encrypt(toFetch.value, toFetch.password);
+    decrypted = CryptoJS.AES.decrypt(toFetch.value, toFetch.password);
+  }
+  else{
+    // encrypted = CryptoJS.AES.encrypt(toFetch.value, "SecretPassphrase");
+    decrypted = CryptoJS.AES.decrypt(toFetch.value, "Secret Passphrase");
+  }
+
+  // console.log(decrypted.toString(CryptoJS.enc.Utf8));
+
+  // console.log(atob(t))
+  // data.data = base64encode(data.data)
+
+  
+
+  const returnData = JSON.stringify(data, null, 2);
+  return new Response(returnData, {
+    headers: reqHeaders
   })
 })
 

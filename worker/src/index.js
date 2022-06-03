b4f4aef2a4986a5f93c5de65dd6afd8abfafa86a
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2022-06-03 23:22:43 +0530

    .....

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,25 +1,29 @@
 import {
   Router
 } from 'itty-router'
-import { customAlphabet  } from 'nanoid';
+import {
+  customAlphabet
+} from 'nanoid';
 
 const nanoid = customAlphabet(
   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
   6,
 );
 
+const headers = {
+  "Content-Type": "application/json; charset=utf-8",
+  'Access-Control-Allow-Credentials': 'true',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type'
+}
+
 const router = Router()
 
 router.get("/ipinfo", (request) => {
   const returnData = JSON.stringify(request.cf, null, 2);
   return new Response(returnData, {
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      'Access-Control-Allow-Credentials': 'true',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type'
-    }
+    headers: headers
   })
 })
 
@@ -28,19 +32,16 @@ router.post('/add', async (request) => {
   let toStore = {}
   if (request.headers.get("Content-Type") === "application/json") {
     data = await request.json()
-    toStore = { key : nanoid(), value : data.data }
+    toStore = {
+      key: nanoid(),
+      value: data.data
+    }
   }
   // await BRAVO68WEB.put(toStore.key, base64encode(toStore.value))
 
   const returnData = JSON.stringify(data, null, 2);
   return new Response(returnData, {
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      'Access-Control-Allow-Credentials': 'true',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type'
-    }
+    headers: headers
   })
 })
 

5853beb15706a568106550448895fea7fb814fa1
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2022-06-04 00:43:18 +0530

    Completed the Worker Module

--- worker/src/index.js
+++ worker/src/index.js
@@ -30,74 +30,92 @@ router.get("/ipinfo", (request) => {
 })
 
 router.post('/add', async (request) => {
-  let data = {}
-  let toStore = {}
-  if (request.headers.get("Content-Type") === "application/json") {
-    data = await request.json()
-    toStore = {
-      key: nanoid(),
-      value: data.data,
-      password: data.password
+  try {
+    let data = {}
+    let toStore = {}
+    if (request.headers.get("Content-Type") === "application/json") {
+      data = await request.json()
+      toStore = {
+        key: nanoid(),
+        value: data.data,
+        password: data.password,
+        readOnce: data.readOnce || false,
+      }
+      if (toStore.readOnce) {
+        toStore.key = "rO-" + toStore.key
+      }
     }
+    var encrypted = ""
+
+    if (toStore.password) {
+      encrypted = CryptoJS.AES.encrypt(toStore.value, toStore.password);
+    } else {
+      encrypted = CryptoJS.AES.encrypt(toStore.value, "SecretPassphrase");
+    }
+
+    await BRAVO68WEB.put(toStore.key, encrypted.toString())
+
+    const returnData = JSON.stringify(toStore, null, 2);
+    return new Response(returnData, {
+      headers: reqHeaders
+    })
+  } catch (err) {
+    console.log(err)
+    return new Response("Error", {
+      headers: reqHeaders
+    })
   }
-  var encrypted = ""
 
-  if(toStore.password){
-    encrypted = CryptoJS.AES.encrypt(toStore.value, toStore.password);
-    // var decrypted = CryptoJS.AES.decrypt(encrypted, toStore.password);
-  }
-  else{
-    encrypted = CryptoJS.AES.encrypt(toStore.value, "SecretPassphrase");
-    // var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
-  }
-
-  // console.log(decrypted.toString(CryptoJS.enc.Utf8));
-
-  // console.log(atob(t))
-  // data.data = base64encode(data.data)
-
-  await BRAVO68WEB.put(toStore.key,encrypted.toString())
-
-  const returnData = JSON.stringify(data, null, 2);
-  return new Response(returnData, {
-    headers: reqHeaders
-  })
 })
 
 router.post('/fetch', async (request) => {
-  let data = {}
-  let toFetch = {}
-  if (request.headers.get("Content-Type") === "application/json") {
-    data = await request.json()
-    toFetch = {
-      key: data.key,
-      password: data.ppassword
+
+  try {
+    let data = {}
+    let toFetch = {}
+    if (request.headers.get("Content-Type") === "application/json") {
+      data = await request.json()
+      toFetch = {
+        key: data.key,
+        password: data.password
+      }
     }
+    
+    var decrypted = ""
+
+    var dataFromCF = await BRAVO68WEB.get(toFetch.key)
+    if (!dataFromCF){
+      throw new Error("Key not found") 
+    }
+
+    if (toFetch.password !== undefined) {
+      decrypted = CryptoJS.AES.decrypt(dataFromCF, toFetch.password);
+    } else {
+      decrypted = CryptoJS.AES.decrypt(dataFromCF, "SecretPassphrase");
+    }
+
+    var toReturn = {
+      data: decrypted.toString(CryptoJS.enc.Utf8)
+    };
+
+    if(toReturn.data === ""){
+      throw new Error("Either the password is wrong or the data is empty")
+    }
+
+    if(toFetch.key.startsWith("rO-")) {
+      await BRAVO68WEB.delete(toFetch.key)
+    }
+
+    const returnData = JSON.stringify(toReturn, null, 2);
+    return new Response(returnData, {
+      headers: reqHeaders
+    })
+  } catch (err) {
+    console.log(err)
+    return new Response(err, {
+      headers: reqHeaders
+    })
   }
-  var decrypted = ""
-
-  await BRAVO68WEB.get(toFetch.key)
-
-  if(toFetch.password){
-    // encrypted = CryptoJS.AES.encrypt(toFetch.value, toFetch.password);
-    decrypted = CryptoJS.AES.decrypt(toFetch.value, toFetch.password);
-  }
-  else{
-    // encrypted = CryptoJS.AES.encrypt(toFetch.value, "SecretPassphrase");
-    decrypted = CryptoJS.AES.decrypt(toFetch.value, "Secret Passphrase");
-  }
-
-  // console.log(decrypted.toString(CryptoJS.enc.Utf8));
-
-  // console.log(atob(t))
-  // data.data = base64encode(data.data)
-
-  
-
-  const returnData = JSON.stringify(data, null, 2);
-  return new Response(returnData, {
-    headers: reqHeaders
-  })
 })
 
 router.all("*", () => new Response("404, not found!", {

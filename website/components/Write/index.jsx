3afe6e3899359040fb37148f637302366a885862
Chirag Bhalotia <me@chirag.codes>
2022-06-06 01:46:33 +0530

    .

--- website/components/Write/index.jsx
+++ website/components/Write/index.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Copy, Lock, Save } from "react-feather";
-import { FireIcon } from '@heroicons/react/solid'
+import { FireIcon } from "@heroicons/react/solid";
 import WriteStyle from "./Write.style";
-
-import axios from "axios";
+import Modal from "./Modal";
+import LockModal from "./LockModal";
+import Axios from "helpers/Axios";
 
 function Write(props) {
     const [value, setValue] = useState("");
     const [count, setCount] = useState(1);
-    const [height, setHeight] = useState("");
+    const [modal, setModal] = useState(false);
+    const [lockModal, setLockModal] = useState(false);
+
     function handleValueChange(evt) {
         const text = evt.target.value;
         const lines = text.split("\n");
@@ -18,32 +21,27 @@ function Write(props) {
         setCount(count);
         setValue(text);
     }
-    function onLock(){}
-    async function onSave(){
-        var data = {
-            "data": value
-        };
-
-        var config = {
-            method: 'post',
-            url: 'http://localhost:8787/add',
-            data : data
-        };
-
-        await axios(config)
-            .then(async function (response) {
-              console.log(response.data);
+    function onLock() {}
+    async function onSave() {
+        // await Axios.post("/add", { data: value })
+        //     .then(async function (response) {
+        //         console.log(response.data);
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     });
+        await Axios.post("/add", { data: "hello" })
+            .then((res) => {
+                console.log(res);
             })
-        .catch(function (error) {
-            console.log(error);
-            });       
+            .catch((err) => {
+                console.error(err);
+            });
     }
-    function onCopy(){}
+    function onCopy() {}
     useEffect(() => {}, []);
     return (
-        <WriteStyle
-            className={"w-screen h-screen flex justify-center items-center "}
-        >
+        <WriteStyle className={"w-screen h-screen flex justify-center items-center "}>
             <div className="writer  bg-amber-50 border-4 border-black rounded-lg ">
                 <div className="heading flex justify-center items-center w-full p-4 bg-amber-100 rounded-t-lg text-black text-xl font-bold relative">
                     <div className="controls flex absolute left-5">
@@ -54,26 +52,40 @@ function Write(props) {
                 </div>
                 <div className="main">
                     <div className="controls-text w-full flex items-center p-5">
-                        <button className="edit p-2 mx-2 rounded-full text-black bg-amber-100 shadow" onClick={onLock}>
+                        <button
+                            className="edit p-2 mx-2 rounded-full text-black bg-amber-100 shadow"
+                            onClick={() => {
+                                setLockModal(true);
+                            }}
+                        >
                             <Lock />
                         </button>
-                        <button className="edit p-2 mx-2 rounded-full text-black bg-amber-100 shadow" onClick={onSave}>
+                        <button
+                            className="edit p-2 mx-2 rounded-full text-black bg-amber-100 shadow"
+                            onClick={onSave}
+                        >
                             <Save />
                         </button>
-                        <button className="edit p-2 mx-2 rounded-full text-black bg-amber-100 shadow" onClick={onCopy}>
+                        <button
+                            className="edit p-2 mx-2 rounded-full text-black bg-amber-100 shadow"
+                            onClick={onCopy}
+                        >
                             <Copy />
                         </button>
-                        <button className="edit p-2 mx-2 rounded-full text-black bg-amber-100 shadow" onClick={onCopy}>
-                            <FireIcon className="h-6 w-5 text-black-1000"/>
+                        <button
+                            className="edit p-2 mx-2 rounded-full text-black bg-amber-100 shadow"
+                            onClick={() => {
+                                setModal(true);
+                            }}
+                        >
+                            <FireIcon className="h-6 w-5 text-black-1000" />
                         </button>
                     </div>
                     <div className="editor">
-                        <div className="line-number border-l-0 border-b-0 border-2 p-2 text-center text-black">
-                            {Array.from(Array(count).keys()).map(
-                                (value, index) => {
-                                    return <div key={value}>{value + 1}</div>;
-                                }
-                            )}
+                        <div className="line-number bg-gray-50 border-l-0 border-b-0 border-2 p-2 text-center text-black">
+                            {Array.from(Array(count).keys()).map((value, index) => {
+                                return <div key={value}>{value + 1}</div>;
+                            })}
                         </div>
                         <div className="text-editor">
                             <textarea
@@ -82,13 +94,15 @@ function Write(props) {
                                 name="content"
                                 id="text-area"
                                 className={
-                                    "h-full w-full bg-transparent text-black outline-none p-2 resize-none"
+                                    "focus:outline-none border-2 focus:border-black h-full w-full bg-transparent text-black outline-none p-2 resize-none"
                                 }
                             ></textarea>
                         </div>
                     </div>
                 </div>
             </div>
+            <Modal open={modal} setOpen={setModal} />
+            <LockModal open={lockModal} setOpen={setLockModal} />
         </WriteStyle>
     );
 }
---
+++ website/helpers/Axios.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "https://tangy-trees-hunt-103-77-137-143.loca.lt/",
+});
